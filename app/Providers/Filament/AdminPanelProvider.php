853605e40bc89ea7605047fd05ff327302f87525
nanu <ranusate19@gmail.com>
2026-04-08 11:23:13 +0800

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,7 +35,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
-            ->brandName("Bidang Akademik 1")
+            ->brandName("Bidang Akademik 1 UNMARIS")
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
